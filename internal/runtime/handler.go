c97698f22c79089dcc18541aea669bd4fc6de1ec
Игорь Самойлов <samoilovip@mail.ru>
2026-07-11 08:27:28 +0300

    runtime: validate batches of events

--- internal/runtime/handler.go
+++ internal/runtime/handler.go
@@ -9,3 +9,8 @@ func Accept(input string) bool {
 	normalized:=strings.TrimSpace(input)
 	return normalized != "" && parser.Parse(normalized)
 }
+
+func AcceptBatch(inputs []string) bool {
+	for _, input := range inputs { if !Accept(input) { return false } }
+	return len(inputs) > 0
+}
